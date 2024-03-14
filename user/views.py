872258b5cf64f7c8181960829da19a2c392e71de
August <giwathankgod404@gmail.com>
2024-03-14 12:02:32 +0100

    feat: Add signup functionality to user authentication

--- user/views.py
+++ user/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import password_validation
+
+from django.contrib.auth.models import User
+from user.models import Customer, Product
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+
+        return render(request, 'signup.html', {'categories': Customer.business_category_choices})
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        business_name = request.POST['business_name']
+        business_category = request.POST['business_category']
+
+        # Creating a user
+        user = User.objects.create(username=username, email=email) # Create a user with the username
+        user.set_password(password) # Assign a password to the user account.
+        user.save() # save to database.
+
+        # Creating a customer
+        customer = Customer.objects.create(
+            user = user,
+            business_name = business_name, 
+            business_category = business_category, 
+        )
+
+        return redirect('signup')
